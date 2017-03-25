97f51509d07dea79c8ce7a6d191f7ec010809394
olcaykaraduman <olckara@gmail.com>
2017-03-25 23:43:08 +0300

    added username and password for mLab

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://heroku_76gt05rt:@ds137530.mlab.com:37530/heroku_76gt05rt");
+mongoose.connect("mongodb://olciniz:qwer1234@ds137530.mlab.com:37530/heroku_76gt05rt");
 
 module.exports = mongoose;
